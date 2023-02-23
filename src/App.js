2186bff0cf718d42d847e02c9857080b75a63f1d
ayeolakenny <ayeolakenny@gmail.com>
2023-02-23 12:41:28 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [website, setWebsite] = useState();
+  const [day, setDay] = useState();
+  const [sites, setSites] = useState([]);
+
+  const handleSubmit = () => {
+    console.log(day);
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    fetch("https://nodejs-mailer002.herokuapp.com/mail/create-site", {
+      method: "POST",
+      headers: myHeaders,
+      body: JSON.stringify({
+        days: Number(day),
+        url: website,
+      }),
+    })
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.log("error", error));
+  };
+
+  useEffect(() => {
+    fetch("https://nodejs-mailer002.herokuapp.com/mail/site", {
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        setSites(result);
+      })
+      .catch((error) => console.log("error", error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <div id="myDIV" className="header">
+        <h2 style={{ margin: "5px" }}>Add Website</h2>
+        <div className="input-container">
+          <input
+            type="text"
+            id="myInput1"
+            placeholder="Website"
+            onChange={(e) => setWebsite(e.target.value)}
+          />
+          <input
+            type="number"
+            id="myInput2"
+            placeholder="Days"
+            style={{ margin: "5px 0" }}
+            onChange={(e) => setDay(e.target.value)}
+          />
+          <span onClick={() => handleSubmit()} className="addBtn">
+            Add
+          </span>
+        </div>
+      </div>
+
+      <ul id="myUL">
+        {sites.length > 0 &&
+          sites.map((result) => (
+            <li>
+              {result.url} remains {result.expires} days to expire
+            </li>
+          ))}
+      </ul>
+    </body>
   );
 }
 
